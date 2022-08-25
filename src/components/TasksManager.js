55604f30898e952474e58f8215caf1b0d838290d
Dawid Drag <dede7d@wp.pl>
2022-08-25 20:52:43 +0200

    added button

--- src/components/TasksManager.js
+++ src/components/TasksManager.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+const apiUrl = ' http://localhost:3000/data'
+
 class TasksManager extends React.Component {
     state = {
         tasks: [],
@@ -7,11 +9,49 @@ class TasksManager extends React.Component {
 
     onClick = () => {
         const { tasks } = this.state;
-        console.log( tasks)
+        console.log(tasks)
     }
 
+    onSubmit = (e) => {
+        e.preventDefault()
+        const { value } = e.target.elements[0]
+        console.log(value)
+
+        const data = {
+            name: 'sdfsdfsd',
+            time: '',
+            isRunning: '',
+            idDone: '',
+            isRemoved: '',
+
+        }
+
+        const options = {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: { 'Content-Type': 'applications/json' }
+        }
+
+        fetch(apiUrl, options)
+            .then(resp => console.log(resp))
+            .catch(err => console.log(err))
+    }
+
+
+
+
     render() {
-        return <h1 onClick={ this.onClick }>TasksManager</h1>
+        return (
+            <>
+                <h1 onClick={this.onClick}>TasksManager</h1>
+
+                <form onSubmit={this.onSubmit}>
+                    <input className='input' name='task' ></input>
+                    <button className='button'>Dodaj zadanie</button>
+                </form>
+
+            </>
+        )
     }
 }
 
